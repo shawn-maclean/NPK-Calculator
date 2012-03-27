e8f4e0e90f7060461f84b96feff3117ef4f54b8c
Shawn MacLean <maclean.shawn@gmail.com>
2012-03-27 15:42:47 -0300

    Layout update #3
    Change-Id: I6579bf61ca8d44cd149b6fc492484134f64048ba
    Signed-off-by: Shawn MacLean <maclean.shawn@gmail.com>

--- NPK-Calculator/src/ca/pe/npkcalculator/NPKCalculatorActivity.java
+++ NPK-Calculator/src/ca/pe/npkcalculator/NPKCalculatorActivity.java
@@ -2,12 +2,41 @@ package ca.pe.npkcalculator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class NPKCalculatorActivity extends Activity {
-    /** Called when the activity is first created. */
+    
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // Create crop selection spinner
+        Spinner cropSelectionSpn = (Spinner) findViewById(R.id.cropSelectionSpn);
+        
+        // Create adapter for crop selection string array
+        ArrayAdapter<CharSequence> cropSelectionAdapter = ArrayAdapter.createFromResource(this, R.array.cropsToSelect, android.R.layout.simple_spinner_item);
+        cropSelectionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        
+        // Set adapter on crop selection spinner
+        cropSelectionSpn.setAdapter(cropSelectionAdapter);        
+     
+        Spinner prevCropSpn = (Spinner) findViewById(R.id.prevCropSpn);        
+        ArrayAdapter<CharSequence> prevCropAdapter = ArrayAdapter.createFromResource(this, R.array.prevCrops, android.R.layout.simple_spinner_item);
+        prevCropAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);        
+        prevCropSpn.setAdapter(prevCropAdapter);
+        
+        Spinner phosphateSpn = (Spinner) findViewById(R.id.phosphateSpn);        
+        ArrayAdapter<CharSequence> phosphateAdapter = ArrayAdapter.createFromResource(this, R.array.soilTestRatings, android.R.layout.simple_spinner_item);
+        phosphateAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);        
+        phosphateSpn.setAdapter(phosphateAdapter);
+        
+        Spinner potashSpn = (Spinner) findViewById(R.id.potashSpn);        
+        ArrayAdapter<CharSequence> potashAdapter = ArrayAdapter.createFromResource(this, R.array.soilTestRatings, android.R.layout.simple_spinner_item);
+        potashAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);        
+        potashSpn.setAdapter(potashAdapter);
+        
     }
 }

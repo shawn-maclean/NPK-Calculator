337416479d6bcdb1a0d521ceff106512c580d6e5
Shawn MacLean <maclean.shawn@gmail.com>
2012-03-27 12:46:32 -0300

    Layout update #2
    Change-Id: I6579bf61ca8d44cd149b6fc492484134f64048ba
    Signed-off-by: Shawn MacLean <maclean.shawn@gmail.com>

--- NPK-Calculator/src/ca/pe/npkcalculator/NPKCalculatorActivity.java
+++ NPK-Calculator/src/ca/pe/npkcalculator/NPKCalculatorActivity.java
@@ -4,7 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class NPKCalculatorActivity extends Activity {
-    /** Called when the activity is first created. */
+    
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
